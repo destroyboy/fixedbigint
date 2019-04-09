c147231b828ebbfe05e9e9b75007f7625eb48959
alexis <binary.physics@gmail.com>
2019-04-09 09:54:24 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/fixedbigint.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fixedbigint.dir/main.c.o"
+  "fixedbigint.pdb"
+  "fixedbigint"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fixedbigint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/fixedbigint.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for fixedbigint.
+# This may be replaced when dependencies are built.
